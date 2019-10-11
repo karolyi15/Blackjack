d50ff4a275a6b6ccfef95a3ab850ee209186544e
karolyi15 <karolyi15@hotmail.com>
2019-10-11 09:48:06 -0600

    imgs files

--- UI.rkt
+++ UI.rkt
@@ -1,4 +1,27 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname UI) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
-;
+;Import librery
+#lang racket/gui
+(require racket/class)
+;*************************************
+;Instantiating a frame
+(define frame(new frame%
+                  [label "BlaCEkJack"]
+                  [width 800]
+                  [height 600]))
+
+;Instantiating a static message
+(define msg(new message%[parent frame]
+                [label "nothing"]))
+
+;Instantiating a button
+(new button%[parent frame]
+     [label "Start Game"]
+     ;procedure for button click
+     [callback (lambda(button event)
+                 (send msg set-label "click"))])
+
+
+
+
+;*************************************
+;Show frame
+(send frame show #t)
